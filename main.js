af5c154141ca1a05c43a6064d6d78d7c7bd006d3
carnagecreations <30275496+carnagecreations@users.noreply.github.com>
2019-12-03 14:33:59 -0700

    numthree

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 let loaf = {
@@ -5,7 +6,23 @@ let loaf = {
   water: 150,
 };
 loaf.hydration = function() {
- return loaf.flour / loaf.water * 100;
+  return loaf.flour / loaf.water * 100;
 };
 
 console.log(loaf.hydration());
+
+const fun = {
+  foo: 'fennecFox',
+  bar: 'macaw',
+  fum: 'monkey',
+  quux: 'owl',
+  spam: 'croc'
+};
+
+for (const property in fun) {
+    console.log(`${property}: ${fun[property]}`);
+}
+
+
+
+  

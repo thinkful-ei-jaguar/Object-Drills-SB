91d99ef2bc4a46599023e07d7f933d2786d0710c
carnagecreations <30275496+carnagecreations@users.noreply.github.com>
2019-12-03 20:00:03 -0700

    seven

--- main.js
+++ main.js
@@ -55,3 +55,43 @@ function createCharecter({name, nickname, race, origin, attack, defense})
 
 
 
+
+//Iterating through objects
+function makeStudentsReport(data) {
+
+  //create an empty array
+  const final = [];
+  //iterate through the entire array
+  for (let i = 0; i < data.length; i++) {
+   //new variable item is assigned to value as passed
+    const item = data[i];
+    final.push(`${item.name}: ${item.grade}`);
+  }
+  return final;
+  }
+
+  function enrollInSummerSchool(students) {
+    //we want to return an array so we must create empty1
+  const final = [];
+  //loop through all of the data
+  for (let i = 0; i < students.length; i++) {
+    //name and course pulled from students data, status is already assigned, it is & pushing 2 final
+    final.push({
+      name: students[i].name, 
+      status: 'In Summer school', 
+  course: students[i].course,
+    }); 
+  }
+  return final;
+  }
+
+  function findById(items, idNum) {
+    //pass through each item
+  for (let i=0; i<items.length; i++) {
+   // if item being passed is = id num, it should return that value
+    if (items[i].id === idNum) {
+    return items[i];}
+  }
+  }
+  
+
